9c91e4a2ff70a8ece65bf073e31107a815825909
Blake <welzie@gmail.com>
2017-11-16 18:29:47 -0800

    Code for the stuff that does things.

--- RestaurantStudio/Program.cs
+++ RestaurantStudio/Program.cs
@@ -6,7 +6,18 @@ namespace RestaurantStudio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Blake's tasty menu!");
+            var menu = new Menu();
+            menu.AddMenuItem(new MenuItem("Hot Dogs", .99, "Mystery meat in link form.", "Main Course", false));
+            menu.AddMenuItem(new MenuItem("Mac n Cheese", 1.99, "Macoroni and cheese powder.", "Side", false));
+            menu.AddMenuItem(new MenuItem("Humus and Pita", 3, "Freshly made humus with pita.", "Appetizer", true));
+
+            foreach(var menuItem in menu.MenuItems)
+            {
+                Console.WriteLine(menuItem);
+            }
+
+            Console.ReadKey();
         }
     }
 }
